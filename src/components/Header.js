5f15e2a0cee4066eb10582bbd98609ba55a37ae3
David Pam <niratpam@gmail.com>
2021-11-02 04:43:01 +0100

    Initial Commit. Download Dependencies

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material';
+const Header = () => {
+  return (
+    <div>
+      <Button variant='outlined'>Nijoe</Button>
+    </div>
+  );
+};
+
+export default Header;
