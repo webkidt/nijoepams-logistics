ee63fdcff3082909eaf5d0e113da5c1434ce8b9e
David Pam <niratpam@gmail.com>
2021-11-24 05:31:25 +0100

    Made AppBar Responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Box,
   AppBar,
@@ -6,15 +7,21 @@ import {
   Container,
   Button,
   Stack,
+  useMediaQuery,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
 } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { withStyles, useTheme } from '@mui/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import HomeIcon from '@mui/icons-material/Home';
+import PhoneIcon from '@mui/icons-material/Phone';
 import ElevateOnScroll from './ElevateOnScroll';
-// import {withStyles} from '@mui/material/styles';
-// import { bgcolor } from '@mui/system';
-// import Image from 'mui-image';
 import bigTitleImage from '../../assets/w1.jpg';
 
-const useStyles = makeStyles({
+const styles = {
   root: {
     flexGrow: 1,
     minHeight: '100vh',
@@ -31,10 +38,23 @@ const useStyles = makeStyles({
   bigTitle: {
     color: '#ffffff',
   },
-});
+};
 
-const Header = (props) => {
-  const classes = useStyles();
+const Header = props => {
+  const { classes } = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box className={classes.root}>
       <ElevateOnScroll {...props}>
@@ -48,15 +68,58 @@ const Header = (props) => {
               >
                 NIJOEPAMS
               </Typography>
-              <Button href='#' sx={{ color: 'common.white' }}>
-                Home
-              </Button>
-              <Button href='#' sx={{ color: 'common.white' }}>
-                Services
-              </Button>
-              <Button href='#' sx={{ color: 'common.white' }}>
-                Contact
-              </Button>
+              {isMobile ? (
+                <>
+                  <IconButton
+                    edge='start'
+                    aria-label='menu'
+                    sx={{ color: theme.palette.primary.contrastText, mr: 2 }}
+                    onClick={handleMenu}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id='menu-appbar'
+                    anchorEl={anchorEl}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    keepMounted
+                    transformOrigin={{vertical: 'top', horizontal: 'right'}}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <ListItemIcon>
+                        <HomeIcon />
+                      </ListItemIcon>
+                      <Typography variant='h6'>Home</Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <ListItemIcon>
+                        <MiscellaneousServicesIcon />
+                      </ListItemIcon>
+                      <Typography variant='h6'>Services</Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <ListItemIcon>
+                        <PhoneIcon />
+                      </ListItemIcon>
+                      <Typography variant='h6'>Contact</Typography>
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <div>
+                  <Button href='#' sx={{ color: 'common.white' }}>
+                    Home
+                  </Button>
+                  <Button href='#' sx={{ color: 'common.white' }}>
+                    Services
+                  </Button>
+                  <Button href='#' sx={{ color: 'common.white' }}>
+                    Contact
+                  </Button>
+                </div>
+              )}
             </Toolbar>
           </Container>
         </AppBar>
@@ -84,7 +147,11 @@ const Header = (props) => {
               Your Swift, Efficient and Reliable Logistics Partner
             </Typography>
             <Box sx={{ width: '100%' }}>
-              <Button variant='contained' color='secondary' sx={{py: 2, px: 4, borderRadius: '20px'}}>
+              <Button
+                variant='contained'
+                color='secondary'
+                sx={{ py: 2, px: 4, borderRadius: '20px' }}
+              >
                 Contact Us
               </Button>
             </Box>
@@ -125,4 +192,4 @@ const Header = (props) => {
   );
 };
 
-export default Header;
+export default withStyles(styles)(Header);
